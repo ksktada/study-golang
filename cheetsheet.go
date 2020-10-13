a0694ff97a6ba4aa046151f5f133b5d58b95427f
ksktada <ksk.tada@gmail.com>
2020-10-13 23:42:44 +0900

    add swap

--- cheetsheet.go
+++ cheetsheet.go
@@ -8,9 +8,16 @@ import (
 func main() {
   fmt.Println("Now you have %g problems.\n", math.Sqrt(7))
   fmt.Println(add(42, 13))
+  a, b := swap("hello", "world")
+  fmt.Println(a, b)
 }
 
+// func
 func add(x int, y int) int {
   return x + y
 }
 
+// multiple results
+func swap(x, y string) (string, string) {
+  return  y, x
+}
