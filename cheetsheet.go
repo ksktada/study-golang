917c5f6057923fcfc58101f2e4d26e98af2b4bf1
ksktada <ksk.tada@gmail.com>
2020-10-14 23:59:00 +0900

    named return value

--- cheetsheet.go
+++ cheetsheet.go
@@ -10,6 +10,7 @@ func main() {
   fmt.Println(add(42, 13))
   a, b := swap("hello", "world")
   fmt.Println(a, b)
+  fmt.Println(split(17))
 }
 
 // func
@@ -20,4 +21,11 @@ func add(x int, y int) int {
 // multiple results
 func swap(x, y string) (string, string) {
   return  y, x
+}
+
+// named return value
+func split(sum int) (x, y int){
+  x = sum * 4 / 9
+  y = sum - x
+  return
 }
