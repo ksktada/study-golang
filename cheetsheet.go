b6ddb7398a3821156e43ea7a701501ba9de100be
ksktada <ksk.tada@gmail.com>
2020-11-27 20:47:57 +0900

    numeric constants

--- cheetsheet.go
+++ cheetsheet.go
@@ -18,6 +18,7 @@ func main() {
   type_conversions()
   type_inference()
   constants()
+  numeric_constants()
 }
 
 // func
@@ -96,3 +97,18 @@ func constants(){
   const Truth = true
   fmt.Println("Go rules?", Truth)
 }
+
+// Numeric Constants
+func numeric_constants(){
+  const(
+    Big = 1 << 100
+    Small = Big >> 99
+  )
+
+  fmt.Println(needInt(Small))
+  fmt.Println(needFloat(Small))
+  fmt.Println(needFloat(Big))
+}
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 { return x * 0.1 }
