46cbdc89e182c9b7faae3a5257fc89f85f63f596
ksktada <ksk.tada@gmail.com>
2020-12-03 23:59:59 +0900

    if and else

--- cheetsheet.go
+++ cheetsheet.go
@@ -23,6 +23,7 @@ func main() {
   for_while()
   fmt.Println(sqrt(2), sqrt(-4))
   fmt.Println(pow(3,2,10), pow(3,3,20))
+  fmt.Println(pow2(3,2,10), pow2(3,3,20))
 }
 
 // func
@@ -155,5 +156,16 @@ func pow(x, n, lim float64) float64 {
   if v:= math.Pow(x, n); v < lim {
     return v
   }
+  return lim
+}
+
+// if and else
+func pow2(x,n, lim float64) float64 {
+  if v := math.Pow(x, n); v < lim {
+    return v
+  } else {
+    fmt.Printf("%g >= %g\n", v, lim)
+  }
+  // can't v here, though
   return lim
 }
